8362484418c2af83c6f72661eb2f0f59b0563c14
Lukas <lukasslezevicius4@gmail.com>
2020-02-05 13:27:58 +0000

    Improved the script.js code

--- speakeasy/app/static/js/script.js
+++ speakeasy/app/static/js/script.js
@@ -1,30 +1,32 @@
 let button = document.getElementById("speech_text_button");
-button.addEventListener("click", handler);
+button.addEventListener("click", click_handler);
 window.addEventListener('keydown', windowKeydownHandler, true);
-let clicks = 0;
+let last_key_was_space = false;
 
 function windowKeydownHandler(e) {
     if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
-        if (e.target.nodeName == 'TEXTAREA' && e.target.type == 'textarea' && e.target.id == "speech_text_input") {
+        if (e.target.id == "speech_text_input") {
             e.preventDefault();
             play_mp3(e.target.value);
-            return false;
         }
     } else if (e.keyCode == 32) {
 		if (e.target.id == "speech_text_input") {
-			//There should be much more checks in order to determine whether to send
-			let words = e.target.value.split(" ");
-			let word = words[words.length - 1];
-			update_suggested_words(word);
-			return false;
+			if (!last_key_was_space) {
+				//The way I deal with symbols and whitespace depends on /suggest spec.
+				let words = e.target.value.split(" ");
+				let user_word = words[words.length - 1];
+				update_suggested_words(user_word);
+				last_key_was_space = true;
+			}
 		}
-    }
+    } else {
+		last_key_was_space = false;
+	}
 }
 
-function handler(e) {
+function click_handler(e) {
     let input_text = document.getElementById("speech_text_input");
     play_mp3(input_text.value);
-    return false;
 }
 
 function play_mp3(val) {
@@ -36,7 +38,7 @@ function play_mp3(val) {
         window.audio.play();
     })
     .catch((error) => {
-        alert("Some error occured");
+        alert("Error: Could not receive the speech audio.");
     });
 }
 
@@ -53,20 +55,10 @@ async function get_mp3(val) {
     return await response.blob();
 }
 
-function update_suggested_words(word) {
-	get_suggested_words(word)
-	.then((wordsJSON) => {
-		let words = wordsJSON["suggested_words"];
-		display_words(words);
-	})
-	.catch((error) => alert("Some other error occured"));
-}
-
-function display_words(words) {
-	for (let i = 1; i <= 3; i++) {
-		let span = document.getElementById("suggest" + String(i));
-		span.textContent = words[i - 1];
-	}
+function update_suggested_words(user_word) {
+	get_suggested_words(user_word)
+	.then((suggested_words) => display_words(suggested_words))
+	.catch((error) => alert("Error: Could not receive suggested words."));
 }
 
 async function get_suggested_words(word) {
@@ -77,5 +69,15 @@ async function get_suggested_words(word) {
 		},
 		body: JSON.stringify({"user_word": word})
 	});
-	return await response.json();
+	return (await response.json())["suggested_words"];
+}
+
+function display_words(words) {
+	/* This function is to be used for displaying new words and showing
+	 * the transition of them becoming ones. Code within the block can be
+	 * removed without consequences. */
+	for (let i = 1; i <= 3; i++) {
+		let span = document.getElementById("suggest" + String(i));
+		span.textContent = words[i - 1];
+	}
 }
