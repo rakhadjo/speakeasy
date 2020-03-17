1067dbcfa40ce9c80941f95ccbd8cf8c6c8ab80e
Yordan Chorbadzhiyski <yordan.chorbadzhiyski@student.manchester.ac.uk>
2020-03-17 13:22:34 +0000

    keybindings with hotkeys

---
+++ script.js
@@ -0,0 +1,100 @@
+window.addEventListener('keydown', windowKeydownHandler, true);
+let last_key_was_space = false;
+
+function windowKeydownHandler(e) {
+    if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
+        if (e.target.id == "speech_text_input") {
+            play_mp3(e.target.value);
+        }
+    } else if (e.keyCode == 32) {
+		if (e.target.id == "speech_text_input") {
+			if (!last_key_was_space) {
+				//The way I deal with symbols and whitespace depends on /suggest spec.
+				let words = e.target.value.split(" ");
+				let user_word = words[words.length - 1];
+				update_suggested_words(user_word);
+				last_key_was_space = true;
+			}
+		}
+	} 
+}
+
+function switchKeyboardNull(i) {
+	if (document.getElementById("keyboard" + i))
+		switchKeyboard(i);
+}
+
+function insert(i) {
+	let keyboard_id = document.getElementsByClassName("active")[0].id; //Assuming no more actives
+	let keyboard_i = Number(keyboard_id[keyboard_id.length - 1]); //keyboards.length <= 5
+	let keyboard = document.getElementById("keyboard" + keyboard_i);
+	let phrase_elements = keyboard.getElementsByClassName("phrase");
+	let phrase = phrase_elements[i].innerHTML;
+	let old_text = document.getElementById("speech_text_input").value;
+	let new_text;
+	if (old_text.length > 0) {
+		new_text = old_text.trim() + " " + phrase + " ";
+	} else {
+		new_text = phrase + " ";
+	}
+	document.getElementById("speech_text_input").value = new_text;
+}
+
+function click_handler(e) {
+    let input_text = document.getElementById("speech_text_input");
+    play_mp3(input_text.value);
+}
+
+function play_mp3(val) {
+    get_mp3(val)
+    .then((blob) => {
+        let url = window.URL.createObjectURL(blob)
+        window.audio = new Audio();
+        window.audio.src = url;
+		window.audio.playbackRate = audio_speed;
+        window.audio.play();
+    })
+    .catch((error) => {
+        alert("Error: Could not receive the speech audio.");
+    });
+}
+
+async function get_mp3(val) {
+    let payload = new FormData();
+    payload.append("speech_text", val);
+    let response = await fetch("/speak", {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify({"speech_text": val})
+    });
+    return await response.blob();
+}
+
+function update_suggested_words(user_word) {
+	get_suggested_words(user_word)
+	.then((suggested_words) => display_words(suggested_words))
+	.catch((error) => alert("Error: Could not receive suggested words."));
+}
+
+async function get_suggested_words(word) {
+	let response = await fetch("/suggest", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({"user_word": word})
+	});
+	return (await response.json())["suggested_words"];
+}
+
+function display_words(words) {
+	/* This function is to be used for displaying new words and showing
+	 * the transition of them becoming ones. Code within the block can be
+	 * removed without consequences. */
+	for (let i = 1; i <= 3; i++) {
+		let span = document.getElementById("suggest" + String(i));
+		span.textContent = words[i - 1];
+	}
+}
